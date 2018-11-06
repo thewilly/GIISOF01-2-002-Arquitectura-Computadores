97ea7935bfdf07182d6092d159bd6e5d9d2a8a56
Willy <10364506+thewilly@users.noreply.github.com>
2018-11-06 12:29:16 +0100

    Update Main.cpp

--- Singlethread/Src/Main.cpp
+++ Singlethread/Src/Main.cpp
@@ -19,13 +19,26 @@ using namespace cimg_library;
  */
 
 int main() {
-	CImg<float> srcImage("bailarina.bmp"); // Open file and object initialization
 
-	float *pRcomp, *pGcomp, *pBcomp; // Pointers to the R, G and B components
+	// IMAGES SETTINGS
+	CImg<float> srcImage1("bailarina.bmp"); // First image
+	CImg<float> srcImage2("figure.png"); // Second image
+
+	// FIRST IMAGE POINTERS
+	float *pRcomp_1, *pGcomp_1, *pBcomp_1; // Pointers to the R, G and B components
+
+	// SECOND IMAGE POINTERS
+	float *pRcomp_2, *pGcomp_2, *pBcomp_2; // Pointers to the R, G and B components
+
+	// NEW IMAGE POINTERS AND ATTRIBUTES
 	float *pRnew, *pGnew, *pBnew;
 	float *pdstImage; // Pointer to the new image pixels
 	int width, height; // Width and height of the image
 	int nComp; // Number of image components
+
+	// BENCHMARK SETTINGS
+	struct timespec tStart, tEnd;
+	double dElapsedTimeS;
 
 
 	/***************************************************
@@ -36,10 +49,17 @@ int main() {
 	 *
 	 */
 
-	srcImage.display(); // If needed, show the source image
-	width = srcImage.width(); // Getting information from the source image
-	height  = srcImage.height();
-	nComp = srcImage.spectrum(); // source image number of components
+	srcImage1.display(); // Show the first image.
+	srcImage2.display(); // show the second image.
+
+	if(srcImage1.height() != srcImage2.height() || srcImage1.width() != srcImage2.width() || srcImage1.spectrum() != srcImage2.spectrum()) {
+		printf("\n Both images must have same dimensions.");
+		//exit(-2);
+	}
+
+	width = srcImage1.width(); // Getting information from the source image
+	height  = srcImage1.height();
+	nComp = srcImage1.spectrum(); // source image number of components
 				// Common values for spectrum (number of image components):
 				//  B&W images = 1
 				//	Normal color images = 3 (RGB)
@@ -53,15 +73,20 @@ int main() {
 		exit(-2);
 	}
 
-	// Pointers to the RGB arrays of the source image
-	pRcomp = srcImage.data(); // pRcomp points to the R component
-	pGcomp = pRcomp + height * width; // pGcomp points to the G component
-	pBcomp = pGcomp + height * width; // pBcomp points to B component
+	// Pointers to the RGB arrays of the source image 1
+	pRcomp_1 = srcImage1.data(); // pRcomp_1 points to the R component
+	pGcomp_1 = pRcomp_1 + height * width; // pGcomp points to the G component
+	pBcomp_1 = pGcomp_1 + height * width; // pBcomp points to B component
+
+	// Pointers to the RGB arrays of the source image 2
+	pRcomp_2 = srcImage2.data(); // pRcomp_2 points to the R component
+	pGcomp_2 = pRcomp_2 + height * width; // pGcomp_2 points to the G component
+	pBcomp_2 = pGcomp_2 + height * width; // pBcomp_2 points to B component
 
 	// Pointers to the RGB arrays of the destination image
 	pRnew = pdstImage;
-	pGnew= pRnew + height * width;
-	pBnew= pGnew + height * width;
+	pGnew = pRnew + height * width;
+	pBnew = pGnew + height * width;
 
 	/*********************************************
 	 * Algorithm start
@@ -72,6 +97,8 @@ int main() {
 	 *
 	 */
 
+	// Start time measurement
+	clock_gettime(CLOCK_REALTIME, &tStart);
 
 
 	/************************************************
@@ -83,9 +110,9 @@ int main() {
 
 	for (int i = 0; i < height ; i++) {
 		for (int j=0; j < width; j++)  {
-			pRnew[i * width + j] = pRcomp[i * width + j];
-			pGnew[i * width + j] = pRcomp[i * width + j];
-			pBnew[i * width + j] = pBcomp[i * width + j];
+			pRnew[i * width + j] = sqrtf( pow(pRcomp_1[i * width + j], 2.0f) + pow(pRcomp_2[i * width + j], 2.0f) ) / sqrtf(2.0f);
+			pGnew[i * width + j] = sqrtf( pow(pGcomp_1[i * width + j], 2.0f) + pow(pGcomp_2[i * width + j], 2.0f) ) / sqrtf(2.0f);
+			pBnew[i * width + j] = sqrtf( pow(pBcomp_1[i * width + j], 2.0f) + pow(pBcomp_2[i * width + j], 2.0f) ) / sqrtf(2.0f);
 		}
 	}
 
@@ -97,6 +124,12 @@ int main() {
 	 * COMPLETE
 	 *
 	 */
+	// Start time measurement
+	clock_gettime(CLOCK_REALTIME, &tEnd);
+
+	dElapsedTimeS = (tEnd.tv_sec - tStart.tv_sec);
+	dElapsedTimeS += (tEnd.tv_nsec - tStart.tv_nsec) / 1e+9;
+	printf("\n Tiempo ejecución: %fs", dElapsedTimeS);
 
 		
 	// Create a new image object with the calculated pixels
@@ -112,5 +145,3 @@ int main() {
 	return(0);
 
 }
-
-
