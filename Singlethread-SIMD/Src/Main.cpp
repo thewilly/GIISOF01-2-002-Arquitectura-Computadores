3e063b9f53864ca930aa91d5257e1f677334bcc7
Willy <10364506+thewilly@users.noreply.github.com>
2018-11-11 23:56:27 +0100

    Update Main.cpp

--- Singlethread-SIMD/Src/Main.cpp
+++ Singlethread-SIMD/Src/Main.cpp
@@ -3,77 +3,160 @@
  *      Author: arias
  */
 
- // Example of use of intrinsic functions using aligned an unaligned arrays
- // This example doensn't include any code about image processing
- // The image processing code have to be added by the students and remove
- // the unnecessary code.
- 
 #include <CImg.h>
 #include <immintrin.h> // Required to use intrinsic functions
 #include <malloc.h>
 #include <stdio.h>
+#include <stdexcept>
 
 using namespace cimg_library;
 
-#define TAM 11 // Array size
+#define SIMD_BANDWITH 8
 
-int main()
-{
-	/********************************
-	 * Here: variable initialization for handling images
-	 */
-	
-	float a[TAM], b[TAM], c[TAM]; // Unaligned arrays of floats in memory
-	
-	// Create three float arrays aligned to 32 bytes memory boundaries
-    float *a_aligned  = (float *)_mm_malloc(sizeof(float) * TAM, sizeof(__m256));
-    float *b_aligned  = (float *)_mm_malloc(sizeof(float) * TAM, sizeof(__m256));
-    float *c_aligned  = (float *)_mm_malloc(sizeof(float) * TAM, sizeof(__m256));
+int main() {
+	// IMAGES SETTINGS
+		CImg<float> srcImage1("bailarina.bmp"); // First image
+		CImg<float> srcImage2("figura.bmp"); // Second image
 
-	__m256 va, vb, vc; // 32 bytes (256 bits) extended CPU registers
+		// FIRST IMAGE POINTERS
+		float *pcompImage1; // Pointer to the pixels of the image 1
 
-	// Initialize unaligned and aligned arrays
-    for (int i = 0; i < TAM; i++)
-    {
-        a[i] = a_aligned[i] = i;      // a = a_aligned = 0, 1, 2, 3, …
-        b[i] = b_aligned[i] = 2 * i;  // b = b_aligned = 0, 2, 4, 6, …
-        c[i] = c_aligned[i] = -1;     // c = c_aligned = -1, -1, -1, …
-    }	
-	
-	// Performs the addition of two aligned vectors, each vector containing 8 floats
-    va = _mm256_load_ps(&a_aligned[0]); // va = a_aligned[0][1]…[7] = 0, 1, 2, 3,  4,  5,  6,  7
-    vb = _mm256_load_ps(&b_aligned[0]); // vb = b_aligned[0][1]…[7] = 0, 2, 4, 6,  8, 10, 12, 14
-    vc = _mm256_add_ps(va, vb);         // vc = va + vb =             0, 3, 6, 9, 12, 15, 18, 21
-	
-	 // Copy the result to the first locations into the c aligned array
-    _mm256_store_ps(&c_aligned[0], vc);
-	
-	// Print the c_aligned array
-    for (int i = 0; i < TAM; i++)
-        printf("\nc_aligned[%d]: %f", i, c_aligned[i]);
+		// SECOND IMAGE POINTERS
+		float *pcompImage2; // Pointer to the pixel of image 2
 
-    printf("\n\n-------------------------------\n");
-	
-	// Now, perform the same additions, but using unaligned arrays (performance decreases)
-    va = _mm256_loadu_ps(&a[0]); // va = a[0][1]…[7] = 0, 1, 2, 3,  4,  5,  6,  7
-    vb = _mm256_loadu_ps(&b[2]); // vb = b[0][1]…[7] = 0, 2, 4, 6,  8, 10, 12, 14
-    vc = _mm256_add_ps(va, vb);  // vc = va + vb     = 0, 3, 6, 9, 12, 15, 18, 21
+		// NEW IMAGE POINTERS AND ATTRIBUTES
+		float *pdstImage; // Pointer to the new image pixels
+		int width, height; // Width and height of the image
+		int nComp; // Number of image components
 
-    // Copy the result to the first locations into the c unaligned array
-    _mm256_storeu_ps(&c[0], vc);
+		// BENCHMARK SETTINGS
+		struct timespec tStart, tEnd;
+		double dElapsedTimeS;
+		const int ALGORITHM_REPETITION_TIMES = 40;
 
-	// Print the c unaligned array
-    for (int i = 0; i < TAM; i++)
-        printf("\nc[%d]: %f", i, c[i]);
-    printf("\n\n-------------------------------\n");
+		// SMID SETTINGS
+		int size = srcImage1._height*srcImage1._width*srcImage1._spectrum;
+		//__m256 a, b, a2, b2, ab2, raizab2, res8;
 
-	// Free memory allocated using _mm_malloc
-	// It has to be freed with _mm_free
-	_mm_free(a_aligned);
-	_mm_free(b_aligned);
-	_mm_free(c_aligned);
-	
-	return(0);
+		__m256 vsqrt2;
+
+		float sqrt2 = sqrtf(2.0f);
+		vsqrt2[0] = sqrt2;
+		vsqrt2[1] = sqrt2;
+		vsqrt2[2] = sqrt2;
+		vsqrt2[3] = sqrt2;
+		vsqrt2[4] = sqrt2;
+		vsqrt2[5] = sqrt2;
+		vsqrt2[6] = sqrt2;
+		vsqrt2[7] = sqrt2;
+
+
+		srcImage1.display(); // Show the first image.
+		srcImage2.display(); // show the second image.
+
+		if (srcImage1.height() != srcImage2.height() || srcImage1.width() != srcImage2.width() || srcImage1.spectrum() != srcImage2.spectrum()) {
+			throw std::domain_error("Both images must have the same size");
+		}
+
+
+		width = srcImage1.width(); // Getting information from the source image
+		height = srcImage1.height();
+		nComp = srcImage1.spectrum(); // source image number of components
+		// Common values for spectrum (number of image components):
+		//  B&W images = 1
+		//	Normal color images = 3 (RGB)
+		//  Special color images = 4 (RGB and alpha/transparency channel)
+
+
+		// Allocate memory space for the pixels of the destination (processed) image
+		pdstImage = (float *) malloc(width * height * nComp * sizeof(float));
+		if (pdstImage == NULL) {
+			throw std::bad_alloc();
+		}
+
+
+		// Pointers to the array of the source image 1
+		pcompImage1 = srcImage1.data();
+
+		// Pointers to the array of the source image 2
+		pcompImage2 = srcImage2.data();
+
+		/*********************************************
+		 * Algorithm start
+		 *
+		 * Measure initial time
+		 */
+		if (clock_gettime(CLOCK_REALTIME, &tStart) < 0) {
+			printf("\n clock_gettime: %d.\n", errno);
+			throw std::runtime_error("Error measuring initial time");
+		}
+
+		/************************************************
+		 * Algorithm.
+		 */
+
+		for (int repetitions = 0; repetitions < ALGORITHM_REPETITION_TIMES; repetitions++) {
+			for (int i = 0; i < size; i += SIMD_BANDWITH) {
+				/*
+					P-SEUDOCODE VERSION (NOT TO INCLUDE IN FINAL RELEASE)
+
+				a 		= 	_mm256_loadu_ps(&pcompImage1[i]);
+				b 		= 	_mm256_loadu_ps(&pcompImage2[i]);
+				a2 		= 	_mm256_mul_ps(a, a);
+				b2 		= 	_mm256_mul_ps(b, b);
+				ab2 	= 	_mm256_add_ps(a2, b2);
+				raizab2 = 	_mm256_sqrt_ps(ab2);
+				res8 	= 	_mm256_div_ps(raizab2, vsqrt2);
+
+				 _mm256_storeu_ps(&pdstImage[i], res8);
+
+				 */
+
+				 _mm256_storeu_ps(&pdstImage[i],
+						 _mm256_div_ps(
+								 _mm256_sqrt_ps(
+										 _mm256_add_ps(
+												 _mm256_mul_ps(
+														 _mm256_loadu_ps(&pcompImage1[i]),
+														 _mm256_loadu_ps(&pcompImage1[i])
+												 ),
+												 _mm256_mul_ps(
+														 _mm256_loadu_ps(&pcompImage2[i]),
+														 _mm256_loadu_ps(&pcompImage2[i])
+												)
+										 )
+								 ),
+								 vsqrt2
+						 )
+				 );
+			}
+		}
+
+		/***********************************************
+		 * End of the algorithm
+		 *
+		 * Measure the final time and calculate the time spent
+		 */
+		// End time measurement
+		if (clock_gettime(CLOCK_REALTIME, &tEnd) < 0) {
+			printf("\n clock_gettime: %d.\n", errno);
+			throw std::runtime_error("Error measuring initial time");
+		}
+		dElapsedTimeS = (tEnd.tv_sec - tStart.tv_sec);
+		dElapsedTimeS += (tEnd.tv_nsec - tStart.tv_nsec) / 1e+9;
+		printf("\n Tiempo ejecución: %fs", dElapsedTimeS);
+		printf("\n Número de repeticiones: %i", ALGORITHM_REPETITION_TIMES);
+		printf("\n Tiempo individual algoritmo: %fs", (dElapsedTimeS / (float) ALGORITHM_REPETITION_TIMES));
+
+		// Create a new image object with the calculated pixels
+		// In case of normal color image use nComp=3,
+		// In case of B&W image use nComp=1.
+		CImg<float> dstImage(pdstImage, width, height, 1, nComp);
+
+		// Store the destination image in disk
+		dstImage.save("bailarina3.bmp");
+
+		// Display the destination image
+		dstImage.display(); // If needed, show the result image
+		return (0);
 }
-
-
